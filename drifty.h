7418e7e60d144da040dbaddd564296154eb709d6
Johan Brinch <zerrez@gmail.com>
2009-07-13 00:08:00 +0200

    common includes

--- drifty.h
+++ drifty.h
@@ -1,6 +1,24 @@
 #ifndef DRIFTY_H
 #define DRIFTY_H
 
-#define u8 unsigned char
+
+#include "skein/skein_port.h"
+
+typedef uint_32t u32b_t;
+
+
+#ifndef FORTUNA_H
+#include "fortuna/fortuna.h"
+#endif
+
+#ifndef ECRYPT_SYNC
+#include "salsa20/ecrypt-sync.h"
+#endif
+
+#ifndef _SKEIN_H
+#include "skein/skein.h"
+#endif
+
+
 
 #endif
