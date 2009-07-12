816b4b807a3e036b6ce8287a2b317de683dc3a4c
Johan Brinch <zerrez@gmail.com>
2009-07-13 00:08:24 +0200

    added initialization and entropy collectors

--- fortuna/fortuna.c
+++ fortuna/fortuna.c
@@ -1,13 +1,94 @@
 #include "fortuna.h"
 
+#include<pthread.h>
+#include<time.h>
+#include<malloc.h>
+#include<string.h>
+
+
+u64b_t time64(int type) {
+	struct timespec ts;
+	clock_gettime(type, &ts);
+	return ts.tv_nsec;
+}
+
+
+u08b_t BUFFER[4096];
+
+
+void rounds(n) {
+	int i;
+	ECRYPT_ctx ctx;
+	ECRYPT_keysetup(&ctx, BUFFER, 256, 8);
+	ECRYPT_ivsetup(&ctx,  BUFFER);
+	for(i = 0; i < n; i++) {
+		ECRYPT_encrypt_bytes(&ctx, BUFFER, BUFFER, 4096);
+	}
+}
+
+
+void fortuna_collector(fortuna_ctx *ctx, int id) {
+	int i; u64b_t rt0,rt1,tr0,tr1,drift;
+	i = 0;
+	id = 1 << id;
+	while(1) {
+		rt0 = time64(CLOCK_MONOTONIC);
+		tr0 = time64(CLOCK_THREAD_CPUTIME_ID);
+		rounds(id);
+		tr1 = time64(CLOCK_THREAD_CPUTIME_ID);
+		rt1 = time64((CLOCK_MONOTONIC));
+
+		u32b_t ent = (rt1-rt0) - (tr1-tr0) & 0xFF;
+		Skein_256_Update(ctx->pools[i]->state, &ent, 1);
+		ctx->pools[i]->updates += 1;
+
+		usleep(id<<18);
+		printf("%u -> %8u\n", id, ctx->pools[i]->updates);
+		i = (i+1) & 0x20;
+	}
+}
+
+void *fortuna_thread(fortuna_thread_ctx *ctx) {
+	//printf("%u\n", ctx->fortuna_ctx->pools[0]->updates);
+	fortuna_collector(ctx->fortuna_ctx, ctx->id);
+}
+
 
 void fortuna_init(fortuna_ctx *ctx, int mask) {
+	u64b_t t = time64(CLOCK_MONOTONIC);
+	u32b_t i;
+	ctx->pools[0]   = calloc(FORTUNA_POOL_NUM  , sizeof(long));
+	ctx->threads[0] = calloc(FORTUNA_THREAD_NUM, sizeof(long));
+	for(i = 0; i < FORTUNA_POOL_NUM; i++) {
+		fortuna_pool_t *pool = malloc(sizeof(fortuna_pool_t));
+		ctx->pools[i] = pool;
+		pool->updates = 0;
+		pool->state = malloc(sizeof(Skein_256_Ctxt_t));
+		Skein_256_Init(pool->state, 256);
+		/** add pool id */
+		Skein_256_Update(pool->state, &i, sizeof(u32b_t));
+		/** add current time */
+		Skein_256_Update(pool->state, &t, sizeof(u64b_t));
+	}
+	if (mask | FORTUNA_INIT_THREADS) {
+		//for(i = ; i < FORTUNA_THREAD_NUM; i++) {
+		for(i = 0; i < 12; i++) {
+			ctx->threads[i] = malloc(sizeof(pthread_t));
+			fortuna_thread_ctx *tctx = malloc(sizeof(tctx));
+			tctx->id = i+1;
+			tctx->fortuna_ctx = ctx;
+			pthread_create(ctx->threads[i], NULL,
+				       fortuna_thread, (void*) tctx);
+		}
+		pthread_join(*ctx->threads[0], NULL);
+	}
 }
 
 
-void fortuna_get(fortuna_ctx *ctx, u8 out[32]) {
+void fortuna_get(fortuna_ctx *ctx, u08b_t out[32]) {
+
 }
 
 
-void fortuna_full(fortuna_ctx *ctx, u8 out[1024]) {
+void fortuna_full(fortuna_ctx *ctx, u08b_t out[1024]) {
 }
--- fortuna/fortuna.h
+++ fortuna/fortuna.h
@@ -6,37 +6,41 @@
 #include<sys/time.h>
 #include<pthread.h>
 
-#include "../skein/skein.h"
 #include "../drifty.h"
 
 
 #define FORTUNA_THREAD_NUM 12
-#define FORTUNA_POOL_NUM 32
+#define FORTUNA_POOL_NUM   32
 
 #define FORTUNA_INIT_THREADS 0x01
 #define FORTUNA_INIT_ALL     0xFF
 
 
 typedef struct {
-	int updates;
-	Skein_256_Ctxt_t state;
-} pool_t;
+	unsigned int updates;
+	Skein_256_Ctxt_t *state;
+} fortuna_pool_t;
 
 typedef struct {
 	struct timeval last_reseed;
-	pool_t  pools[FORTUNA_POOL_NUM];
-	pthread_t threads[FORTUNA_THREAD_NUM];
+	fortuna_pool_t *pools[FORTUNA_POOL_NUM];
+	pthread_t      *threads[FORTUNA_THREAD_NUM];
 } fortuna_ctx ;
+
+typedef struct {
+	unsigned int id;
+	fortuna_ctx *fortuna_ctx;
+} fortuna_thread_ctx ;
 
 
 /** initialize PRNG */
 void fortuna_init(fortuna_ctx *ctx, int mask);
 
 /** get 256-bits output from PRNG */
-void fortuna_get(fortuna_ctx *ctx, u8 *out);
+void fortuna_get(fortuna_ctx *ctx, u08b_t out[32]);
 
 /** get full 32*256 bit state */
-void fortuna_extract(fortuna_ctx *ctx, u8 *out);
+void fortuna_extract(fortuna_ctx *ctx, u08b_t *out[1024]);
 
 
 #endif
