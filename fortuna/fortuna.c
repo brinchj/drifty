0253523011789fce49f029c946bb4560167cd7ba
Johan Brinch <zerrez@gmail.com>
2009-07-12 20:47:45 +0200

    added fortuna stubbed impl

---
+++ fortuna/fortuna.c
@@ -0,0 +1,13 @@
+#include "fortuna.h"
+
+
+void fortuna_init(fortuna_ctx *ctx, int mask) {
+}
+
+
+void fortuna_get(fortuna_ctx *ctx, u8 out[32]) {
+}
+
+
+void fortuna_full(fortuna_ctx *ctx, u8 out[1024]) {
+}
