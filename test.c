442a7767d0f5f2af5aaacee82a94cde6ff99b3fc
Johan Brinch <zerrez@gmail.com>
2009-07-14 21:32:02 +0200

    updates in test file

--- test.c
+++ test.c
@@ -1,13 +1,21 @@
 #include<stdio.h>
 #include<malloc.h>
+#include<time.h>
+
 #include "common.h"
 #include "fortuna/fortuna.h"
+
 
 int main() {
 
 	fortuna_ctx *ctx = malloc(sizeof(ctx));
 	fortuna_init(ctx, 0);
 
-	printf("hello world.\nl");
+	int i;
+	for(i = 0; i < 1000; i++) {
+		printf("%d -> %2d updates.\n", i, ctx->pools[0]->updates);
+		sleep(1);
+	}
+
 	return 0;
 }
