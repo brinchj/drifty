5318fa7df02a42b3133a117ce4e38d8bf1f3f946
Johan Brinch <zerrez@gmail.com>
2009-07-16 11:43:24 +0200

    much better and faster initialization; comments++

--- drifty.c
+++ drifty.c
@@ -2,58 +2,56 @@
 #include<stdio.h>
 
 void drifty_init(drifty_ctx *ctx) {
-	printf("> initializing ");
+	int i,j;
+
+	/** randomize counter offset */
 	ctx->count = 0;
-	fortuna_init(&ctx->fortuna_ctx, FORTUNA_INIT_ALL);
-	/** wait for some entropy to arrive */
-	int prev = -1;
-	int cur ;
-	while((cur = ctx->fortuna_ctx.pools[0]->updates) < 64) {
-		sleep(1);
-		if(cur != prev) {
-			prev = cur;
-			printf(".");
-			fflush(stdout);
-		}
-	}
-	printf("\n");
+	for(i = 0; i < 8; i++)
+		ctx->count = (ctx->count << 8) | fortuna_getbyte();
 
-	u08b_t buffer[BLOCK_SIZE];
-	fortuna_full(&ctx->fortuna_ctx, buffer);
-	int i;
-	for(i = 0; i < 1024; i++)
-		ctx->state[i % STATE_SIZE] ^= buffer[i];
-
-	/** warm up the prng state */
-	printf("> mixing state ");
-	for(i = 0; i < 64; i++) {
-		sleep(1);
-		drifty_block(ctx, buffer);
-		printf(".");
+	/** fill state with entropy */
+	printf("> collect entropy    |");
+	for(j = 0; j < 64; j++) {
+		usleep(10000);
+		for(i = 0; i < STATE_SIZE; i++)
+			ctx->state[i] ^= fortuna_getbyte();
+		printf("=");
 		fflush(stdout);
 	}
-	printf("\n");
+	printf("|\n");
+
+	/** initialize entropy collection */
+	fortuna_init(&ctx->fortuna_ctx, FORTUNA_INIT_ALL);
+
+	/** warm up the prng state */
+	u08b_t buffer[BLOCK_SIZE];
+	for(i = 0; i < 64; i++) {
+		drifty_block(ctx, buffer);
+	}
 }
 
 
 void drifty_block(drifty_ctx *ctx, u08b_t *out) {
+	u08b_t buffer[STATE_SIZE + BLOCK_SIZE];
+
+	/** compute expanded hash of counter and internal state */
 	Skein1024_Ctxt_t state;
 	Skein1024_Init(&state, STATE_SIZE*8 + BLOCK_SIZE*8);
 	Skein1024_Update(&state, (u08b_t*) &ctx->count, sizeof(u64b_t));
 	Skein1024_Update(&state, ctx->state, STATE_SIZE);
-
-	u08b_t buffer[STATE_SIZE + BLOCK_SIZE];
 	Skein1024_Final(&state, buffer);
 
-
+	/** encrypt hash with Salsa20 and change key */
 	ECRYPT_encrypt_blocks(&ctx->stream_ctx, buffer, buffer,
 			      (STATE_SIZE+BLOCK_SIZE)/64);
 	ECRYPT_keysetup(&ctx->stream_ctx, buffer, 256, 64);
 	ECRYPT_ivsetup(&ctx->stream_ctx, &buffer[32]);
 
+	/** overwrite state, output and buffer */
 	memcpy(buffer, &ctx->state,         STATE_SIZE);
 	memcpy(out,    &buffer[STATE_SIZE], BLOCK_SIZE);
 	memset(buffer, 0, STATE_SIZE + BLOCK_SIZE);
 
+	/** increment state counter */
 	ctx->count += 1;
 }
