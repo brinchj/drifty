3eb6f193b24f31f93f617de366021a74ad6577af
Johan Brinch <zerrez@gmail.com>
2009-07-17 13:27:03 +0200

    critical bug fixes

--- drifty.c
+++ drifty.c
@@ -16,12 +16,14 @@ void drifty_init(drifty_ctx *ctx) {
 		usleep(10000);
 		// add entropy
 		for(i = 0; i < STATE_SIZE; i++)
-			ctx->state[i] ^= fortuna_getbyte();
+			ctx->state[i] ^= fortuna_getwbyte();
 		// mix state
 		drifty_block(ctx, buffer);
-		printf("\r> collect entropy    %.2f", ((float)j)/STATE_SIZE);
+		drifty_block(ctx, buffer);
+		printf("\r> collect entropy    %.3f", ((float)j)/STATE_SIZE);
 		fflush(stdout);
 	}
+	printf("\r> collect entropy    %.3f", 1.0);
 	printf("\n");
 
 	/** initialize entropy collection */
@@ -45,18 +47,20 @@ void drifty_block(drifty_ctx *ctx, u08b_t *out) {
 	Skein1024_Update(&state, ctx->state, STATE_SIZE);
 	Skein1024_Final(&state, buffer);
 
-	/** encrypt hash with Salsa20 and change key */
-	ECRYPT_encrypt_bytes(&ctx->stream_ctx, buffer, buffer,
-			      (STATE_SIZE/2));
+	/** update state and output */
+	const int flag = ctx->count & 1;
+
+	/** encrypt non-overwritten state */
+	u08b_t *st = &buffer[(!flag) * STATE_SIZE/2];
+	ECRYPT_encrypt_blocks(&ctx->stream_ctx, st, st, (STATE_SIZE/2)/64);
+	memcpy(buffer, &ctx->state[flag * STATE_SIZE/2], STATE_SIZE/2);
+	memcpy(out,    &buffer[STATE_SIZE/2], BLOCK_SIZE);
+
+	/** change Salsa20 key */
 	u08b_t key_iv[64];
-	ECRYPT_encrypt_bytes(&ctx->stream_ctx, key_iv, key_iv, 64);
+	ECRYPT_encrypt_blocks(&ctx->stream_ctx, key_iv, key_iv, 1);
 	ECRYPT_keysetup(&ctx->stream_ctx, key_iv, 256, 64);
 	ECRYPT_ivsetup(&ctx->stream_ctx, &key_iv[32]);
-
-	/** update state and output */
-	int offset = (ctx->count & 1) * STATE_SIZE/2;
-	memcpy(buffer, &ctx->state[offset],   STATE_SIZE/2);
-	memcpy(out,    &buffer[STATE_SIZE/2], BLOCK_SIZE);
 
 	/** cleanup */
 	memset(buffer, 0, sizeof(buffer));
